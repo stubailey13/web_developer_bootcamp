b02ac73239c2f210f119bf00eba91f89dd9df86a
stubailey13 <46968730+stubailey13@users.noreply.github.com>
2019-06-01 10:06:55 +0200

    Add files via upload

---
+++ javaScript/stalkerJS/Stalker_Script.js
@@ -0,0 +1,8 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var userAge = prompt("How old are you?");
+
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("You are " +userAge + " years old");
+
+
